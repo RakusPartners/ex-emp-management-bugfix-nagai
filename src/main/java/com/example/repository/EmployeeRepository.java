5d29e1f23ab18ea0e0a370ca82fde67011476808
nagai <zhishengzhangjing@gmail.com>
2024-05-28 13:50:25 +0900

    commit3

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -65,7 +65,7 @@ public class EmployeeRepository {
 	 * @exception org.springframework.dao.DataAccessException 従業員が存在しない場合は例外を発生します
 	 */
 	public Employee load(Integer id) {
-		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count FROM employees WHERE id=:id ORDER BY hire_date";
+		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count FROM employees WHERE id=:id ";
 
 		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 
